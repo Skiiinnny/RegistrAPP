4a71f41540e62c500cd46753653a302a879155cd
Cristopher <cristophergamboa@outlook.com>
2021-09-15 19:03:36 -0300

    Login v0.2

--- RegistrAPP/src/app/home/home.page.ts
+++ RegistrAPP/src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -11,13 +13,26 @@ export class HomePage implements OnInit {
 
   user = {nombre: "", clave: ""}
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toastController: ToastController) {}
 
-  login(){
-    let navigationExtras: NavigationExtras = {
-      state: {user: this.user}
-      };
-      this.router.navigate(['/dashboard-alumno'],navigationExtras);      
+  async login(){
+    if(this.user.nombre != "" && this.user.clave != ""){
+      let navigationExtras: NavigationExtras = {
+        state: {user: this.user}
+        };
+
+        this.router.navigate(['/dashboard-alumno'],navigationExtras);
+
+        this.user = {nombre: "", clave: ""}
+    }     
+    else{
+      const toast = await this.toastController.create({
+        message: '¡Llene todos los campos!',
+        duration: 1000,
+        color: 'warning'
+      });
+      toast.present();
+    }
   }
 
   ngOnInit(){

0991d7c91707c98544691940c87e0a47332c42f6
Skiiinnny <nataniel.soto.pulido@gmail.com>
2021-11-07 19:25:13 -0300

    Config para la implementación de la camara

--- RegistrAPP/src/app/recovery/recovery.page.ts
+++ RegistrAPP/src/app/recovery/recovery.page.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AnimationController, ToastController } from '@ionic/angular';
-
+import { SQLite } from '@ionic-native/sqlite/ngx';
 
 @Component({
   selector: 'app-recovery',
@@ -9,14 +9,15 @@ import { AnimationController, ToastController } from '@ionic/angular';
   styleUrls: ['./recovery.page.scss'],
 })
 export class RecoveryPage implements OnInit {
-
-  constructor(private toastController: ToastController,
-    private router: Router, private animationCtrl: AnimationController) { }
+  constructor(
+    private toastController: ToastController,
+    private router: Router,
+    private animationCtrl: AnimationController
+  ) {}
 
   username = '';
 
   async envio() {
-
     if (this.username != '') {
       const toast = await this.toastController.create({
         message: '¡Correo de recuperación enviado!',
@@ -24,18 +25,20 @@ export class RecoveryPage implements OnInit {
         color: 'success',
       });
       toast.present();
+     
       this.router.navigate(['/home']);
     } else {
-      this.animationCtrl.create()
+      this.animationCtrl
+        .create()
         .addElement(document.querySelector('#input'))
         .duration(300)
         .keyframes([
-          { offset: 0.25, 'transform': 'translate(10px)' },
-          { offset: 0.75, 'transform': 'translate(-10px)' },
-          { offset: 1, 'transform': 'translate(0px)' },
+          { offset: 0.25, transform: 'translate(10px)' },
+          { offset: 0.75, transform: 'translate(-10px)' },
+          { offset: 1, transform: 'translate(0px)' },
         ])
         .iterations(2)
-        .play()
+        .play();
       const toast = await this.toastController.create({
         message: '¡Llene todos los campos!',
         duration: 2000,
@@ -45,7 +48,5 @@ export class RecoveryPage implements OnInit {
     }
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- RegistrAPP/src/app/scan-qr/scan-qr.page.ts
+++ RegistrAPP/src/app/scan-qr/scan-qr.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimationController } from '@ionic/angular';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-scan-qr',
@@ -7,22 +8,38 @@ import { AnimationController } from '@ionic/angular';
   styleUrls: ['./scan-qr.page.scss'],
 })
 export class ScanQrPage implements OnInit {
+  imgURL;
+  constructor(private animationCtrl: AnimationController, private camera: Camera) {}
 
-  constructor(private animationCtrl: AnimationController) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  animar() {
+    this.animationCtrl
+      .create()
+      .addElement(document.querySelector('#texto'))
+      .duration(200)
+      .keyframes([
+        { offset: 0.25, transform: 'translate(10px)' },
+        { offset: 0.75, transform: 'translate(-10px)' },
+        { offset: 1, transform: 'translate(0px)' },
+      ])
+      .iterations(2)
+      .play();
+    
   }
-  
-  animar(){
-    this.animationCtrl.create()
-    .addElement(document.querySelector('#texto'))
-    .duration(200)
-    .keyframes([
-      {offset: 0.25, 'transform': 'translate(10px)'},
-      {offset: 0.75, 'transform': 'translate(-10px)'},
-      {offset: 1, 'transform': 'translate(0px)'},
-    ])
-    .iterations(2)
-    .play()
+  getFromCamera() {
+
+    this.camera.getPicture({
+
+      sourceType : this.camera.PictureSourceType.CAMERA,
+
+      destinationType: this.camera.DestinationType.DATA_URL
+
+    }).then((imageData) => {
+
+      this.imgURL = 'data:image/jpeg;base64,' + imageData;
+
+    });
+
   }
 }
